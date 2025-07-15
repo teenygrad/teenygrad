e80863e52c5bfc6533ef1ee19c2c8657006ec8fe
Arshad Mahmood <1391251+arshadm@users.noreply.github.com>
2025-07-15 12:12:59 +0100

    added basic setup script for ubuntu

---
+++ setup_ubuntu.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+# install build tools
+sudo apt-get install build-essential
+
+RELOGIN_REQUIRED=false
+
+# checkif rustc is installed, if not install it
+if ! command -v rustc &> /dev/null
+then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    . "$HOME/.cargo/env"
+
+    cargo install cargo-expand
+    cargo install cargo-watch
+
+    RELOGIN_REQUIRED=true
+else
+    echo "Rust is installed, updating it"
+    rustup update
+fi
+
+
+if [ "$RELOGIN_REQUIRED" = true ]; then
+    echo "Please logout and login again, as new tools are installed"
+    exit 0
+fi
