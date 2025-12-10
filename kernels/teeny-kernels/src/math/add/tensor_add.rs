b40e63cbf9ff643bda777be41be7c3adce83e243
Arshad Mahmood <1391251+arshadm@users.noreply.github.com>
2025-12-10 15:51:36 +0000

    wip triton llvm api

--- kernels/teeny-kernels/src/math/add/tensor_add.rs
+++ kernels/teeny-kernels/src/math/add/tensor_add.rs
@@ -36,7 +36,7 @@ pub fn tensor_add<'a, T: Triton + 'a, D: types::Dtype, const BLOCK_SIZE: u32>(
     let offsets = T::arange(0, BLOCK_SIZE) + block_start;
 
     // // Create a mask to handle cases where n_elements is not divisible by BLOCK_SIZE
-    // let mask = Some(offsets.slt(n_elements));
+    let mask = Some(offsets.slt(n_elements));
 
     // // Load data from global memory with masking
     // let x = T::load(&x_ptr.add_offsets(&offsets), &mask);
